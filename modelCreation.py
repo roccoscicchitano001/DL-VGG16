062f64d8928e88034690202d76d86ed30c21b01c
Rocco <rocco.scicchitano@gpi.it>
2024-02-23 20:48:30 +0100

    Correzione su modelCreation

--- modelCreation.py
+++ modelCreation.py
@@ -1,43 +1,50 @@
 import numpy as np
-from sklearn.metrics import classification_report
 import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
 from keras.layers import Flatten, Dense, Dropout
 from keras.applications import VGG16
 
-# Aggiunto il batch size
+# Imposta il batch size per il training
 batch_size = 32
 
-# Imposta il numero desiderato di epoche
-epochs = 5
+# Imposta il numero desiderato di epoche per l'addestramento
+epochs = 15
 
-# Directory principale di addestramento
+# Directory principale dei dati di addestramento
 train_data_dir = r'set/train/train'
 
-# Directory principale di test
+# Directory principale dei dati di test
 test_data_dir = r'set/test'
 
-train_datagen = ImageDataGenerator(rescale = 1./255,
-                                   rotation_range = 45,
+# Configura il generatore di immagini per il training con diverse trasformazioni
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   rotation_range=45,
                                    horizontal_flip=True,
-                                   shear_range = 0.3,validation_split=0.2,
-                                   zoom_range = 0.3)
+                                   shear_range=0.3,
+                                   validation_split=0.2,
+                                   zoom_range=0.3)
 
+# Crea il generatore di dati di addestramento utilizzando il percorso specificato
 train_data = train_datagen.flow_from_directory(train_data_dir,
-                                              target_size = (100, 100), 
-                                              class_mode = 'categorical', batch_size = 64, subset="training")
+                                              target_size=(100, 100),
+                                              class_mode='categorical',
+                                              batch_size=64,
+                                              subset="training")
 
+# Crea il generatore di dati di validazione utilizzando lo stesso percorso e suddivisione
 valid_data = train_datagen.flow_from_directory(train_data_dir,
-                                              target_size = (100, 100), 
-                                              class_mode = 'categorical', batch_size = 64, subset="validation")
+                                              target_size=(100, 100),
+                                              class_mode='categorical',
+                                              batch_size=64,
+                                              subset="validation")
 
-vgg16 = VGG16(input_shape = (100,100, 3), weights = 'imagenet', include_top = False)
-
+# Carica il modello VGG16 pre-addestrato e congela i pesi dei suoi strati
+vgg16 = VGG16(input_shape=(100, 100, 3), weights='imagenet', include_top=False)
 for layer in vgg16.layers:
-  layer.trainable = False
+    layer.trainable = False
 
-# Mappa delle classi
+# Mappa delle classi ottenuta dal generatore di dati di addestramento
 class_mapping = train_data.class_indices
 print(f'Etichette: {class_mapping}')
 
@@ -45,25 +52,28 @@ print(f'Etichette: {class_mapping}')
 num_classes = len(class_mapping)
 print(f'Numero classi: {num_classes}')
 
+# Aggiungi strati personalizzati al modello VGG16
 flatten = Flatten()(vgg16.output)
-
-dense = Dense(256, activation = 'relu')(flatten)
+dense = Dense(256, activation='relu')(flatten)
 dense = Dropout(0.5)(dense)
-dense = Dense(100, activation = 'relu')(dense)
+dense = Dense(100, activation='relu')(dense)
 dense = Dropout(0.3)(dense)
 
-# Output Layer
-prediction = Dense(33, activation = 'softmax')(dense)
+# Strato di output con attivazione softmax per classificazione multiclasse
+prediction = Dense(num_classes, activation='softmax')(dense)
 
-model = Model(inputs = vgg16.input, outputs = prediction)
+# Crea il modello finale utilizzando il modello VGG16 come base
+model = Model(inputs=vgg16.input, outputs=prediction)
 
+# Visualizza la struttura del modello
 model.summary()
 
-# Compile the Model
-model.compile(loss = 'categorical_crossentropy', metrics = ['accuracy'], optimizer='adam')
+# Compila il modello specificando la funzione di loss, metriche e ottimizzatore
+model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='adam')
 
-# Fit the model
-history = model.fit_generator(train_data,validation_data=valid_data,epochs=15)
+# Addestra il modello utilizzando i generatori di dati di addestramento e validazione
+history = model.fit_generator(train_data, validation_data=valid_data, epochs=epochs)
 
-keras_file="Model.h5"
+# Salva il modello addestrato
+keras_file = "Model.h5"
 model.save(keras_file)
